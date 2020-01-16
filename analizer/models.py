dbf270678fb4532be94322e3a2ecf74fcc91fde0
Alexandre Siaux <asiaux@z4r5p4.le-101.fr>
2020-01-16 11:52:18 +0100

    toto

--- analizer/models.py
+++ analizer/models.py
@@ -1,4 +1,6 @@
 from djongo import models
+from calendar import monthrange
+from math import pi, pow
 from datetime import datetime
 from .production_models import \
 	InfoGrume, \
@@ -14,14 +16,39 @@ from .production_models import \
 	InfoTempsDeCycleSciage
 
 
-class CampagneManager(models.DjongoManager):
+def datecheck(year, month, day, d=0) -> tuple:
+	day += 1
+	if day >= monthrange(year, month)[1]:
+		day = 1
+		month += 1
+	if month > 12:
+		month = 1
+		year += 1
+	return (year, month, day)
 
-	def time(self, name=None, day=1):
-		return super().get_queryset().using('data').filter(
+
+class CampagneQuerySet(models.QuerySet):
+	def day(self, name: str, day=1, month=1, year=2019):
+		year2, month2, day2 = datecheck(year, month, day)
+		return self.filter(
 			entreprise=name,
-			temps_de_cycle__gte={"time": datetime(2019, 10, day, 00, 00, 00).isoformat()},
-			temps_de_cycle__lt={"time": datetime(2019, 10, day + 1, 00, 00, 00).isoformat()}
+			temps_de_cycle__gte={"time": datetime(year, month, day, 00, 00, 00).isoformat()},
+			temps_de_cycle__lt={"time": datetime(year2, month2, day2, 00, 00, 00).isoformat()}
 		)
+
+
+class CampagneManager(models.DjongoManager):
+	def get_queryset(self):
+		return CampagneQuerySet(self.model, using='data')
+
+	def prod_day(self, name=None, day=1, month=1, year=2019):
+		c = 0
+		query = self.get_queryset().day(name=name, day=day, month=month, year=year)
+		for x in query:
+			dcubage = (x.mesure_grume.diametre_cubage_mm / 10) / 2
+			lcubage = x.mesure_grume.longueur_cubage_mm / 10
+			c += pi * pow(dcubage, 2) * lcubage
+		return {'vcube': c / 1000000,  'label':{'horizontal': 'toto'}}
 
 
 class Campagne(models.Model):
